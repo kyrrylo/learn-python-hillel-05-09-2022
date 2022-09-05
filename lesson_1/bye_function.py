626d49eea75b33462b01223e0aa34cd362be27dd
kyrrylo <kyrrylo@gmail.com>
2022-09-05 21:34:02 +0300

    приклад імпорту з запитань учнів

---
+++ lesson_1/bye_function.py
@@ -0,0 +1,6 @@
+def print_bye(name):
+    print(f"Bye, {name}")
+
+
+if __name__ == '__main__':
+    print('I am from another file')
---
+++ lesson_1/lesson_1.py
@@ -0,0 +1,22 @@
+from bye_function import print_bye
+
+# Version Control System = VCS
+
+
+# def - объявление функции
+def print_hi(hi_name):
+    print(f'Hello, {hi_name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print("x: How are you?")
+    print('y: I\'m fine')
+    print(x)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    name = "kyrylo"
+    x = 5
+    print_hi(name)
+    print(f'How is weather, {name}?')
+    print_bye(name)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
